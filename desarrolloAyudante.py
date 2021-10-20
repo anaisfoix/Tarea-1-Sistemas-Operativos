f1e26046dee1814fffead3543425828fa178a7c9
anaisfoix <foixanais@gmail.com>
2021-10-20 18:39:41 -0300

    Third commit

---
+++ desarrolloAyudante.py
@@ -0,0 +1,90 @@
+import pygame
+from math import floor
+from time import sleep
+from threading import Semaphore, Thread
+
+
+colores = {
+    ' ' : (255,255,255), 
+    'X' : (0,0,0), 
+    'V' : (0,255,255), 
+    'B' : (255,255,0), 
+    'C' : (255,0,255), 
+}
+
+
+def actualiza_laberinto():
+    for i in range(0, filas):
+        for j in range(0, columnas):
+            dibuja_cuadrado(i, j, colores[matriz[i][j]])
+    pygame.display.update()
+
+def dibuja_cuadrado(y, x, color):
+    pygame.draw.rect(screen, color, pygame.Rect(150 + x*ancho + 1, 100 + y*largo +1, ancho, largo))
+
+
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 600
+
+filas = 50
+columnas = 30
+filename = 'inputLaberinto.txt'
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+# Fondo blanco
+screen.fill([255,255,255])
+
+pygame.display.update()
+
+# El contorno del laberinto
+pygame.draw.rect(screen, (230,30,30), pygame.Rect(150,100,400,400), 1)
+
+# Ancho y largo de los cuadros dentro de la matriz
+ancho = 400/columnas
+largo = 400/filas
+
+# Lineas horizontales
+for i in range(1, filas):
+        pygame.draw.line(screen, (230, 30, 30), (150,100 + i*largo), (400+150,100 +  i*largo), 1)
+
+# Para líneas verticales
+for i in range(1, columnas):
+    pygame.draw.line(screen, (230, 30, 30), (150 + i*ancho, 100), (150 +  i*ancho, 100 + 400), 1)
+
+
+pygame.display.update()
+
+
+file = open(filename)
+lineas = file.readlines()
+
+# Creamos la matriz
+matriz = []
+for i in range(filas):
+    matriz.append([' '] * 30)
+
+
+# Colocamos las paredes y la ventana
+for linea in lineas:
+    datos = linea.split(',')
+    columna = int(datos[0])
+    fila = int(datos[1])
+    dato = datos[2]
+    matriz[fila][columna] = dato[0]
+
+run = True
+#####################################################
+#         ACÁ CREAR LA LÓGICA DE LOS HILOS          #
+#####################################################
+
+# Loop principal de pygame. Es necesario para que windows no crea que no responde.
+while run:
+    actualiza_laberinto()
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
